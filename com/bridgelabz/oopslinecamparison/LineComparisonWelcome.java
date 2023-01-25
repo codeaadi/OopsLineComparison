3ef65dce328b300986b4ef22c6bb94880642664d
codeaadi <adityadubey362@gmail.com>
2023-01-25 09:40:27 +0530

    Added:Welcome Message for Line Comparison Program

---
+++ com/bridgelabz/oopslinecamparison/LineComparisonWelcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.oopslinecamparison;
+
+public class LineComparisonWelcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Opps Method");
+    }
+}
